597a20a7f41b10772dbb36528f2c269d464fda7e
Omer Itzhaki <itzhaki320@gmail.com>
2022-07-10 23:25:49 +0300

    Added Logger.

--- cron.py
+++ cron.py
@@ -4,11 +4,11 @@ from datetime import date, timedelta
 from blackbox import do_transaction
 from models import Payments, Loans, PaymentStatus, PaymentType
 from routes import SRC_BANK_ACCOUNT
-from server import db
+from server import db, logger
 from sqlalchemy import desc
 
 # Constants
-now = str(date.today() + timedelta(days=7))
+NOW = str(date.today() + timedelta(days=7))
 
 
 def delay_payment(payment, loan_id):
@@ -17,20 +17,22 @@ def delay_payment(payment, loan_id):
     delay = Payments(id=uuid.uuid4().hex, loan_id=loan_id, transaction_id=None, amount=payment.amount,
                      status=PaymentStatus.PENDING, direction=PaymentType.DEBIT, due_date=new_debit)
     db.session.add(delay)
+    logger.info(f"New payment date added to debit/s: {loan_id}")
 
 
 def download_report():
     columns = ['Transaction ID', 'Loan ID', 'Due Date', 'Direction', 'Status']
-    with open(f'{date.today()}.csv', 'w', newline="") as f:
+    with open(f'{NOW}.csv', 'w', newline="") as f:
         writer = csv.writer(f)
         writer.writerow(columns)
-        for i in Payments.query.filter_by(due_date=now):
+        for i in Payments.query.filter_by(due_date=NOW):
             payment = i.transaction_id, i.loan_id, i.due_date, i.direction, i.status
             writer.writerow(payment)
+        logger.info(f"A daily report is created {NOW}")
 
 
 def collector():
-    payments = Payments.query.filter_by(due_date=now)
+    payments = Payments.query.filter_by(due_date=NOW)
     for payment in payments:
         loan = Loans.query.filter_by(id=payment.loan_id).first()
         try:
@@ -38,12 +40,13 @@ def collector():
                                                     amount=payment.amount, direction=payment.direction)
             payment.status = PaymentStatus.SUCCEEDED
             loan.weeks_payed += 1
+            logger.info(f"Payments were successfully collected")
         except Exception as e:
             print(str(e))
             payment.status = PaymentStatus.FAILED
             payment.transaction_id = None
             delay_payment(payment=payment, loan_id=loan.id)
-
+            logger.info(f"The payment failed{payment.loan_id}")
         finally:
             db.session.commit()
     download_report()
--- server.py
+++ server.py
@@ -1,8 +1,17 @@
+import logging
 import local_secrets
 import models
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from routes import routes
+
+# Logger
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s ")
+handler = logging.FileHandler("Processor.log")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 # CONSTANTS
 HOST = local_secrets.host
